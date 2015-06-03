0ce01763b8a753b2c6c89c6a676d92f1ca0df9d1
Power Snail <powersnail@gmail.com>
2015-06-03 18:45:10 +0800

    First Commit

---
+++ ClassAutoScheduler/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAutoScheduler
+{
+    class Course
+    {
+        List<Session> sessions;
+
+        public int NumSession
+        {
+            get
+            {
+                return sessions.Count;
+            }
+        }
+
+        public void addSession(Session _session)
+        {
+            sessions.Add(_session);
+        }
+    }
+}
